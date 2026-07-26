fb799174e41cf24a2643d08c28e4a50ae3ab6716
victorcotrimgb <cotvictor21@gmail.com>
2026-07-26 14:25:56 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,14 +5,4 @@ export const firebaseConfig = {
   storageBucket: "lista-de-compras-4d377.firebasestorage.app",
   messagingSenderId: "57976357516",
   appId: "1:57976357516:web:c0e2aae1025b2625eff1a9"
-};// Cole aqui a configuração do seu aplicativo Web criada no Firebase Console.
-// Firebase Console > Configurações do projeto > Seus apps > Web app > SDK setup and configuration
-
-export const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI.firebaseapp.com",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI.firebasestorage.app",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
 };
